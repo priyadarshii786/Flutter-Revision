20bac2b0b016b2223b5b8f05c309a488649c4429
priyadarshii786 <tathagatp918@gmail.com>
2024-01-04 22:41:08 +0530

    upto lect 29

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,20 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        backgroundColor: Color.fromARGB(255, 125, 66, 227),
-        body: Center(
-          child: Text('Hello Chikku!!'),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 26, 2, 80),
+                Color.fromARGB(255, 45, 7, 98),
+              ],
+            ),
+          ),
+          child: const Center(
+            child: Text('Hello Chikku!!'),
+          ),
         ),
       ),
     ),
