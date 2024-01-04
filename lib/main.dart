23e841cd5ba7c30916d834b2005bc897ae3e575f
priyadarshii786 <tathagatp918@gmail.com>
2024-01-04 22:56:35 +0530

    upto lect 32

--- lib/main.dart
+++ lib/main.dart
@@ -11,10 +11,18 @@ void main() {
                 Color.fromARGB(255, 26, 2, 80),
                 Color.fromARGB(255, 45, 7, 98),
               ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
             ),
           ),
           child: const Center(
-            child: Text('Hello Chikku!!'),
+            child: Text(
+              'Hello Chikku!!',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 28,
+              ),
+            ),
           ),
         ),
       ),
