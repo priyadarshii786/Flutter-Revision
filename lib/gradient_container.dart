4b2f5a4a43bc1d528f89bd7af3965033e081e1d2
priyadarshii786 <tathagatp918@gmail.com>
2024-01-05 01:51:47 +0530

    upto lect 39; intro and basic demonstration of variables.

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,20 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:udemy_tutorial/styled_text.dart';
 
+
+var startAlignment = Alignment.topLeft;  // ----here we have declared two variables and are using them in the code ahead.
+var endAlignment = Alignment.bottomRight;
+
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [
+          colors: const [
             Color.fromARGB(255, 26, 2, 80),
             Color.fromARGB(255, 45, 7, 98),
           ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+          begin: startAlignment, // ----- here we are using those two variables which we have declared above in the code.
+          end: endAlignment,
         ),
       ),
       child: const Center(
