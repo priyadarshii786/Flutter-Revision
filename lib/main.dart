a1c60cb63f1acbb6bcf7ee19b56f7d0b03c7a5f7
priyadarshii786 <tathagatp918@gmail.com>
2024-01-04 21:52:50 +0530

    upto lect 28

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 125, 66, 227),
         body: Center(
           child: Text('Hello Chikku!!'),
         ),
