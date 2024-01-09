c54aa40f1e083cfa967287691e0b88f2ff34a167
priyadarshii786 <tathagatp918@gmail.com>
2024-01-10 01:07:44 +0530

    upto lect 41; about final and const variable.

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:udemy_tutorial/styled_text.dart';
 
+// ----- using final as a variable/data container means ki later on in the code this data container will never receive a new value
+// final startAlignment = Alignment.topLeft; 
+// final endAlignment = Alignment.bottomRight;
 
-var startAlignment = Alignment.topLeft;  // ----here we have declared two variables and are using them in the code ahead.
-var endAlignment = Alignment.bottomRight;
+const startAlignment = Alignment.topLeft; 
+const endAlignment = Alignment.bottomRight; // ---- const is same as final but the thing is it provides some more information to dart when used instead of final. And it is advisable to use "const" everywhere.
 
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
@@ -11,9 +14,9 @@ class GradientContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(
+      decoration: const BoxDecoration(
         gradient: LinearGradient(
-          colors: const [
+          colors: [
             Color.fromARGB(255, 26, 2, 80),
             Color.fromARGB(255, 45, 7, 98),
           ],
